3ba128bd45e38e8fc29081ccdf21413e199da8a1
calvi <calvin@brianandkathi.com>
2024-06-02 23:05:13 -0400

    Added RPGLRaceTest.

---
+++ Test/com/rpglc/core/RPGLRaceTest.cs
@@ -0,0 +1,47 @@
+﻿using com.rpglc.core;
+using com.rpglc.core.mocks;
+using com.rpglc.database;
+using com.rpglc.json;
+using com.rpglc.testutils;
+
+namespace Test.com.rpglc.core;
+
+[AssignDatabase]
+[Collection("Serial")]
+public class RPGLRaceTest {
+
+    [ExtraRacesMock]
+    [ClearDatabaseAfterTest]
+    [Fact(DisplayName = "levels up RPGLObject")]
+    public void LevelsUpRPGLObject() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        RPGLRace rpglRace = RPGLFactory.GetRace("test:race_with_leveled_features");
+
+        rpglRace.LevelUpRPGLObject(rpglObject, new JsonObject().LoadFromString("""
+            {
+                "Effect Choice": [ 1 ]
+            }
+            """), 1);
+
+        List<RPGLEffect> effects = DBManager.QueryRPGLEffects(x => x.Target == rpglObject.GetUuid());
+        Assert.Equal(2, effects.Count);
+        Assert.Equal("test:dummy", effects[0].GetDatapackId());
+        Assert.Equal("test:dummy", effects[1].GetDatapackId());
+
+        JsonArray events = rpglObject.GetEvents();
+        Assert.Equal(1, events.Count());
+        Assert.Equal("test:dummy", events.GetString(0));
+
+        JsonArray resources = rpglObject.GetResources();
+        Assert.Equal(2, resources.Count());
+        Assert.Equal(
+            "test:dummy",
+            DBManager.QueryRPGLResource(x => x.Uuid == resources.GetString(0)).GetDatapackId()
+        );
+        Assert.Equal(
+            "test:dummy",
+            DBManager.QueryRPGLResource(x => x.Uuid == resources.GetString(1)).GetDatapackId()
+        );
+    }
+
+};
---
+++ Test/com/rpglc/core/mocks/ExtraRacesMock.cs
@@ -0,0 +1,53 @@
+﻿using com.rpglc.database;
+using com.rpglc.testutils;
+using System.Reflection;
+
+namespace com.rpglc.core.mocks;
+
+public class ExtraRacesMock : DefaultMock {
+
+    public override void Before(MethodInfo methodUnderTest) {
+        base.Before(methodUnderTest);
+
+        DBManager.InsertRPGLRace(new json.JsonObject().LoadFromString("""
+            {
+                "metadata": {
+                    "author": "Calvin Withun"
+                },
+                "name": "Race With Leveled Features",
+                "description": "A race designed for testing racial level-up features.",
+                "datapack_id": "test:race_with_leveled_features",
+                "ability_score_bonuses": { },
+                "features": {
+                    "1": {
+                        "gain": {
+                            "effects": [
+                                "test:dummy",
+                                {
+                                    "name": "Effect Choice",
+                                    "count": 1,
+                                    "options": [
+                                        "does-not-exist",
+                                        "test:dummy",
+                                        "does-not-exist"
+                                    ]
+                                }
+                            ],
+                            "events": [
+                                "test:dummy"
+                            ],
+                            "resources": [
+                                "test:dummy",
+                                {
+                                    "resource": "test:dummy",
+                                    "count": 1
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+            """));
+    }
+
+};
