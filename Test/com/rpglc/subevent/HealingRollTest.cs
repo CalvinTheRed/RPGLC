756756665806fc49e30089c4e077dda358d07c50
calvi <calvin@brianandkathi.com>
2024-06-18 23:13:20 -0400

    Added HealingRollTest.

---
+++ Test/com/rpglc/subevent/HealingRollTest.cs
@@ -0,0 +1,312 @@
+﻿using com.rpglc.core;
+using com.rpglc.json;
+using com.rpglc.testutils;
+using com.rpglc.testutils.mocks;
+
+namespace com.rpglc.subevent;
+
+[AssignDatabase]
+[Collection("Serial")]
+public class HealingRollTest {
+
+    [DefaultMock]
+    [ClearDatabaseAfterTest]
+    [Fact(DisplayName = "prepares with no healing")]
+    public void PreparesWithNoHealing() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        HealingRoll healingRoll = new HealingRoll()
+            .SetSource(rpglObject)
+            .Prepare(new DummyContext(), new());
+
+        Assert.Equal("""[]""", healingRoll.GetHealing().ToString());
+    }
+
+    [DefaultMock]
+    [ClearDatabaseAfterTest]
+    [DieTestingMode]
+    [Fact(DisplayName = "prepares with healing")]
+    public void PreparesWithHealing() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        HealingRoll healingRoll = new HealingRoll()
+            .JoinSubeventData(new JsonObject()
+                /*{
+                    "healing": [
+                        {
+                            "bonus": 1,
+                            "dice": [
+                                { "size": 6, "determined": [ 3 ] }
+                            ]
+                        }
+                    ]
+                }*/
+                .PutJsonArray("healing", new JsonArray()
+                    .AddJsonObject(new JsonObject()
+                        .PutLong("bonus", 1)
+                        .PutJsonArray("dice", new JsonArray()
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(3)
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+            .SetSource(rpglObject)
+            .Prepare(new DummyContext(), new());
+
+        Assert.Equal("""
+            [
+              {
+                "bonus": 1,
+                "dice": [
+                  {
+                    "determined": [ ],
+                    "roll": 3,
+                    "size": 6
+                  }
+                ]
+              }
+            ]
+            """,
+            healingRoll.GetHealing().PrettyPrint()
+        );
+    }
+
+    [DefaultMock]
+    [ClearDatabaseAfterTest]
+    [DieTestingMode]
+    [Fact(DisplayName = "rerolls healing dice")]
+    public void RerollsHealingDice() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        HealingRoll healingRoll = new HealingRoll()
+            .JoinSubeventData(new JsonObject()
+                /*{
+                    "healing": [
+                        {
+                            "bonus": 1,
+                            "dice": [
+                                { "size": 6, "determined": [ 1, -1 ] },
+                                { "size": 6, "determined": [ 3, 4, -1 ] },
+                                { "size": 6, "determined": [ 6, -1 ] }
+                            ]
+                        }
+                    ]
+                }*/
+                .PutJsonArray("healing", new JsonArray()
+                    .AddJsonObject(new JsonObject()
+                        .PutLong("bonus", 1)
+                        .PutJsonArray("dice", new JsonArray()
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(1)
+                                    .AddLong(-1)
+                                )
+                            )
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(3)
+                                    .AddLong(4)
+                                    .AddLong(-1)
+                                )
+                            )
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(6)
+                                    .AddLong(-1)
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+            .SetSource(rpglObject)
+            .Prepare(new DummyContext(), new());
+
+        healingRoll.RerollHealingDice(2, 5);
+
+        Assert.Equal("""
+            [
+              {
+                "bonus": 1,
+                "dice": [
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 1,
+                    "size": 6
+                  },
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 4,
+                    "size": 6
+                  },
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 6,
+                    "size": 6
+                  }
+                ]
+              }
+            ]
+            """,
+            healingRoll.GetHealing().PrettyPrint()
+        );
+    }
+
+    [DefaultMock]
+    [ClearDatabaseAfterTest]
+    [DieTestingMode]
+    [Fact(DisplayName = "sets healing dice")]
+    public void SetsHealingDice() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        HealingRoll healingRoll = new HealingRoll()
+            .JoinSubeventData(new JsonObject()
+                /*{
+                    "healing": [
+                        {
+                            "bonus": 1,
+                            "dice": [
+                                { "size": 6, "determined": [ 1, -1 ] },
+                                { "size": 6, "determined": [ 3, -1 ] },
+                                { "size": 6, "determined": [ 6, -1 ] }
+                            ]
+                        }
+                    ]
+                }*/
+                .PutJsonArray("healing", new JsonArray()
+                    .AddJsonObject(new JsonObject()
+                        .PutLong("bonus", 1)
+                        .PutJsonArray("dice", new JsonArray()
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(1)
+                                    .AddLong(-1)
+                                )
+                            )
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(3)
+                                    .AddLong(-1)
+                                )
+                            )
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(6)
+                                    .AddLong(-1)
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+            .SetSource(rpglObject)
+            .Prepare(new DummyContext(), new());
+
+        healingRoll.SetHealingDice(6, 2, 5);
+
+        Assert.Equal("""
+            [
+              {
+                "bonus": 1,
+                "dice": [
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 1,
+                    "size": 6
+                  },
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 6,
+                    "size": 6
+                  },
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 6,
+                    "size": 6
+                  }
+                ]
+              }
+            ]
+            """,
+            healingRoll.GetHealing().PrettyPrint()
+        );
+    }
+
+    [DefaultMock]
+    [ClearDatabaseAfterTest]
+    [DieTestingMode]
+    [Fact(DisplayName = "maximizes healing dice")]
+    public void MaximizesTypedDamageDice() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        HealingRoll healingRoll = new HealingRoll()
+            .JoinSubeventData(new JsonObject()
+                /*{
+                    "healing": [
+                        {
+                            "bonus": 1,
+                            "dice": [
+                                { "size": 6, "determined": [ 1, -1 ] }
+                            ]
+                        }
+                    ]
+                }*/
+                .PutJsonArray("healing", new JsonArray()
+                    .AddJsonObject(new JsonObject()
+                        .PutLong("bonus", 1)
+                        .PutJsonArray("dice", new JsonArray()
+                            .AddJsonObject(new JsonObject()
+                                .PutLong("size", 6)
+                                .PutJsonArray("determined", new JsonArray()
+                                    .AddLong(1)
+                                    .AddLong(-1)
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+            .SetSource(rpglObject)
+            .Prepare(new DummyContext(), new());
+
+        healingRoll.MaximizeHealingDice();
+
+        Assert.Equal("""
+            [
+              {
+                "bonus": 1,
+                "dice": [
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 6,
+                    "size": 6
+                  }
+                ]
+              }
+            ]
+            """,
+            healingRoll.GetHealing().PrettyPrint()
+        );
+    }
+
+};
