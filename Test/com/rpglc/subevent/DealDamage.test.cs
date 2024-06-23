be7a6fdf69815fcca48dafff94de62dcd5705585
calvi <calvin@brianandkathi.com>
2024-06-23 11:16:26 -0400

    Implemented DealDamage subevent.
    Updated unit tests.

---
+++ Test/com/rpglc/subevent/DealDamage.test.cs
@@ -0,0 +1,94 @@
+﻿using com.rpglc.core;
+using com.rpglc.database;
+using com.rpglc.json;
+using com.rpglc.testutils.beforeaftertestattributes;
+using com.rpglc.testutils.beforeaftertestattributes.mocks;
+using com.rpglc.testutils.core;
+
+namespace com.rpglc.subevent;
+
+[AssignDatabase]
+[Collection("Serial")]
+public class DealDamageTest {
+
+    [ClearDatabaseAfterTest]
+    [DefaultMock]
+    [DieTestingMode]
+    [Fact(DisplayName = "prepares")]
+    public void Prepares() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        DealDamage dealDamage = new DealDamage()
+            .JoinSubeventData(new JsonObject().LoadFromString("""
+                {
+                    "damage": [
+                        {
+                            "damage_type": "fire",
+                            "formula": "range",
+                            "bonus": 1,
+                            "dice": [
+                                { "count": 1, "size": 6, "determined": [ 3, -1 ] }
+                            ]
+                        }
+                    ]
+                }
+                """))
+            .SetSource(rpglObject)
+            .Prepare(new DummyContext(), new());
+
+        Assert.Equal("""
+            [
+              {
+                "bonus": 1,
+                "damage_type": "fire",
+                "dice": [
+                  {
+                    "determined": [
+                      -1
+                    ],
+                    "roll": 3,
+                    "size": 6
+                  }
+                ],
+                "scale": {
+                  "denominator": 1,
+                  "numerator": 1,
+                  "round_up": false
+                }
+              }
+            ]
+            """,
+            dealDamage.json.GetJsonArray("damage").PrettyPrint());
+    }
+
+    [ClearDatabaseAfterTest]
+    [DefaultMock]
+    [DieTestingMode]
+    [Fact(DisplayName = "deals damage")]
+    public void DealsDamage() {
+        RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", "Player 1");
+        DealDamage dealDamage = new DealDamage()
+            .JoinSubeventData(new JsonObject().LoadFromString("""
+                {
+                    "damage": [
+                        {
+                            "damage_type": "fire",
+                            "formula": "range",
+                            "bonus": 1,
+                            "dice": [
+                                { "count": 1, "size": 6, "determined": [ 3, -1 ] }
+                            ]
+                        }
+                    ]
+                }
+                """))
+            .SetSource(rpglObject)
+            .Prepare(new DummyContext(), new())
+            .SetTarget(rpglObject)
+            .Invoke(new DummyContext(), new());
+
+        rpglObject = DBManager.QueryRPGLObject(x => x._id == rpglObject.GetId());
+
+        Assert.Equal(1000 - 1 - 3, rpglObject.GetHealthCurrent());
+    }
+
+};
