69cad66bb71a625a4fcf8291095dd385586cc4db
calvi <calvin@brianandkathi.com>
2025-03-30 14:41:38 -0400

    Add unit tests for GrantSkillExpertise, GrantSkillProficiency, and GrantSkillHalfProficiency

--- Test/com/rpglc/condition/CheckSkill.test.cs
+++ Test/com/rpglc/condition/CheckSkill.test.cs
@@ -1,9 +1,7 @@
-﻿using com.rpglc.core;
-using com.rpglc.json;
+﻿using com.rpglc.json;
 using com.rpglc.testutils.beforeaftertestattributes;
 using com.rpglc.testutils.beforeaftertestattributes.mocks;
 using com.rpglc.testutils.core;
-using com.rpglc.testutils;
 using com.rpglc.testutils.subevent;
 using com.rpglc.subevent;
 
