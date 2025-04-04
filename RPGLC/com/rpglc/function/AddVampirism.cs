04460c6ba8651cda0ae9f418ff05838bcb761026
calvi <calvin@brianandkathi.com>
2025-04-03 22:52:28 -0400

    Clean up vampirism additions.

--- RPGLC/com/rpglc/function/AddVampirism.cs
+++ RPGLC/com/rpglc/function/AddVampirism.cs
@@ -10,7 +10,10 @@ public class AddVampirism : Function {
 
     public override void Run(RPGLEffect? rpglEffect, Subevent subevent, JsonObject functionJson, RPGLContext context, JsonArray originPoint) {
         if (subevent is IVampiricSubevent vampiricSubevent) {
-            JsonArray vampirismArray = functionJson.GetJsonArray("vampirism");
+            JsonArray vampirismArray = functionJson.GetJsonArray("vampirism") ?? new JsonArray().AddJsonObject(new());
+            if (vampirismArray.IsEmpty()) {
+                vampirismArray.AddJsonObject(new());
+            }
             for (int i = 0; i < vampirismArray.Count(); i++) {
                 vampiricSubevent.AddVampirism(subevent, vampirismArray.GetJsonObject(i));
             }
--- docs/function/AddVampirism.md
+++ docs/function/AddVampirism.md
@@ -11,6 +11,8 @@
 
 Applies a list of vampiric behaviors to a damage-dealing subevent.
 
+Note that the field `"vampirism"` is optional, and will default to a value of `[{}]`, which will go on to become the default vampirism addition of `[ { "damage_type": "*", "scale": { "numerator": 1, "denominator": 1, "round_up": false } } ]`.
+
 ###### Applicable Subevents
 - `AttackRoll`
 - `DealDamage`
