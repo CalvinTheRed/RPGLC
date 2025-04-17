04255d03eaa1a6a4dd5f0b3a65060d559396194e
calvi <calvin@brianandkathi.com>
2025-04-17 18:12:06 -0400

    Added InvokeSubevents function.
    Updated unit tests.

---
+++ RPGLC/com/rpglc/function/InvokeSubevents.cs
@@ -0,0 +1,55 @@
+﻿using com.rpglc.core;
+using com.rpglc.json;
+using com.rpglc.subevent;
+
+namespace com.rpglc.function;
+
+/// <summary>
+///   Invokes subevents.
+///   
+///   <code>
+///   {
+///     "function": "invoke_subevents",
+///     "subevents": [
+///       &lt;subevent instructions&gt;
+///     ],
+///     "source": {
+///       "from": "subevent" | "effect",
+///       "object": "source" | "target",
+///       "as_origin": &lt;bool = false&gt;
+///     },
+///     "targets": [
+///       {
+///         "from": "subevent" | "effect",
+///         "object": "source" | "target",
+///         "as_origin": &lt;bool = false&gt;
+///       }
+///     ]
+///   }
+///   </code>
+/// </summary>
+public class InvokeSubevents : Function {
+
+    public InvokeSubevents() : base("invoke_subevents") { }
+
+    public override void Run(RPGLEffect? rpglEffect, Subevent subevent, JsonObject functionJson, RPGLContext context, JsonArray originPoint) {
+        JsonArray nestedSubeventJsonArray = functionJson.GetJsonArray("subevents") ?? new();
+        RPGLObject source = RPGLEffect.GetObject(rpglEffect, subevent, functionJson.GetJsonObject("source"));
+        List<RPGLObject> targets = functionJson.GetJsonArray("targets").AsList()
+            .Select(targetJson => RPGLEffect.GetObject(rpglEffect, subevent, new JsonObject((Dictionary<string, object>) targetJson)))
+            .ToList();
+
+        for (int i = 0; i < nestedSubeventJsonArray.Count(); i++) {
+            JsonObject nestedSubeventJson = nestedSubeventJsonArray.GetJsonObject(i);
+            Subevent nestedSubevent = Subevent.Subevents[nestedSubeventJson.GetString("subevent")]
+                .Clone(nestedSubeventJson)
+                .SetSource(source)
+                .SetOriginItem(rpglEffect.GetOriginItem())
+                .Prepare(context, originPoint, rpglEffect);
+            foreach (RPGLObject target in targets) {
+                nestedSubevent.Clone().SetTarget(target).Invoke(context, originPoint, rpglEffect);
+            }
+        }
+    }
+
+};
---
+++ Test/com/rpglc/function/InvokeSubevents.test.cs
@@ -0,0 +1,70 @@
+﻿using com.rpglc.core;
+using com.rpglc.json;
+using com.rpglc.subevent;
+using com.rpglc.testutils;
+using com.rpglc.testutils.beforeaftertestattributes;
+using com.rpglc.testutils.beforeaftertestattributes.mocks;
+using com.rpglc.testutils.core;
+using com.rpglc.testutils.subevent;
+
+namespace com.rpglc.function;
+
+[Collection("Serial")]
+[RPGLInitTesting]
+public class InvokeSubeventsTest {
+
+    [ClearRPGLAfterTest]
+    [DefaultMock]
+    [Fact(DisplayName = "invokes subevents")]
+    public void InvokesSubevents() {
+        RPGLObject actingObject = RPGLFactory.NewObject("test:dummy", TestUtils.USER_ID);
+        RPGLObject respondingObject = RPGLFactory.NewObject("test:dummy", TestUtils.USER_ID);
+
+        RPGLContext context = new DummyContext()
+            .Add(actingObject)
+            .Add(respondingObject);
+
+        RPGLEffect rpglEffect = RPGLFactory.NewEffect("test:dummy")
+            .SetSource(respondingObject.GetUuid())
+            .SetTarget(respondingObject.GetUuid());
+
+        Subevent subevent = new DummySubevent()
+            .SetSource(actingObject)
+            .SetTarget(respondingObject);
+
+        JsonObject functionJson = new JsonObject().LoadFromString("""
+            {
+                "function": "invoke_subevents",
+                "subevents": [
+                    {
+                        "subevent": "deal_damage",
+                        "damage": [
+                            {
+                                "formula": "number",
+                                "damage_type": "fire",
+                                "number": 10
+                            }
+                        ]
+                    }
+                ],
+                "source": {
+                    "from": "effect",
+                    "object": "target",
+                    "as_origin": false
+                },
+                "targets": [
+                    {
+                        "from": "subevent",
+                        "object": "source",
+                        "as_origin": false
+                    }
+                ]
+            }
+            """);
+
+        new InvokeSubevents().Execute(rpglEffect, subevent, functionJson, context, new());
+
+        Assert.Equal(1000 - 10, actingObject.GetHealthCurrent());
+    }
+
+};
