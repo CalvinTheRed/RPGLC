e61df38139088e8c355bf0f0cadf6ad2b53a1801
calvi <calvin@brianandkathi.com>
2025-11-02 10:37:23 -0500

    Add support for event groups.

--- RPGLC/com/rpglc/data/TO/RPGLEventTemplateTO.cs
+++ RPGLC/com/rpglc/data/TO/RPGLEventTemplateTO.cs
@@ -6,13 +6,15 @@ public class RPGLEventTemplateTO : DatabaseContentTO {
     public Dictionary<string, object> AreaOfEffect { get; set; }
     public List<object> Cost { get; set; }
     public List<object> Subevents { get; set; }
+    public string? Group { get; set; }
     public string? OriginItem { get; set; }
 
     public RPGLEventTemplate ToTemplate() {
         RPGLEventTemplate template = new();
         template.PutJsonObject("area_of_effect", new JsonObject(AreaOfEffect));
-        template.PutJsonArray("cost", new json.JsonArray(Cost));
-        template.PutJsonArray("subevents", new json.JsonArray(Subevents));
+        template.PutJsonArray("cost", new JsonArray(Cost));
+        template.PutJsonArray("subevents", new JsonArray(Subevents));
+        template.PutString("group", Group);
         template.PutString("origin_item", OriginItem);
         
         ApplyToTemplate(template);
