05d8f0d6b6f5ac1f7b88e12adea85e8e50436954
calvi <calvin@brianandkathi.com>
2026-01-04 10:39:23 -0500

    Updated GrantDisadvantage to be stateful. Updated unit tests.

--- Test/com/rpglc/function/GrantDisadvantage.test.cs
+++ Test/com/rpglc/function/GrantDisadvantage.test.cs
@@ -1,32 +1,33 @@
 ﻿using com.rpglc.core;
 using com.rpglc.json;
+using com.rpglc.runtime;
 using com.rpglc.subevent;
 using com.rpglc.testutils.core;
-using com.rpglc.testutils.subevent;
 
 namespace com.rpglc.function;
 
 [Collection("Serial")]
 public class GrantDisadvantageTest {
 
-    [Fact(DisplayName = "grants disadvantage")]
-    public void GrantsDisadvantage() {
-        Subevent subevent = new DummyRollSubevent()
-            .Prepare(new DummyContext(), new());
+    [Fact(DisplayName = "sets crit on hit")]
+    public void SetsCritOnHit() {
+        RPGLContext context = new DummyContext();
+        RPGLEffect rpglEffect = new();
+        Subevent subevent = new AttackRoll();
 
-        new GrantDisadvantage().Execute(
-            new RPGLEffect(),
-            subevent,
-            new JsonObject().LoadFromString("""
-                {
-                    "function": "grant_disadvantage"
-                }
-                """),
-            new DummyContext(),
-            new()
-        );
+        GrantDisadvantage grantDisadvantage = new();
 
-        Assert.True(subevent.json.GetBool("has_disadvantage"));
+        JsonObject functionJson = new JsonObject().LoadFromString("""
+            {
+                "function": "grant_disadvantage"
+            }
+            """);
+
+        FunctionState.StateData result;
+
+        result = grantDisadvantage.functionSteps[0](rpglEffect, subevent, functionJson, context);
+        Assert.Equal(new FunctionState.StateData() { dependency = null, stepCompleted = true }, result);
+        Assert.True((subevent as AttackRoll).json.GetBool("has_disadvantage"));
     }
 
 };
