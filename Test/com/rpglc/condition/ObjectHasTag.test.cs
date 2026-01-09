a90f2aee737b9618e3b14e9eefd9db32439878cd
calvi <calvin@brianandkathi.com>
2026-01-09 18:42:09 -0500

    Updated ObjectHasTag to be stateful. Updated unit tests.

--- Test/com/rpglc/condition/ObjectHasTag.test.cs
+++ Test/com/rpglc/condition/ObjectHasTag.test.cs
@@ -1,9 +1,11 @@
 ﻿using com.rpglc.core;
 using com.rpglc.json;
+using com.rpglc.runtime;
+using com.rpglc.subevent;
+using com.rpglc.testutils;
 using com.rpglc.testutils.beforeaftertestattributes;
 using com.rpglc.testutils.beforeaftertestattributes.mocks;
 using com.rpglc.testutils.core;
-using com.rpglc.testutils;
 using com.rpglc.testutils.subevent;
 
 namespace com.rpglc.condition;
@@ -11,43 +13,45 @@ namespace com.rpglc.condition;
 [Collection("Serial")]
 public class ObjectHasTagTest {
 
-    [Fact(DisplayName = "condition mismatch")]
-    public void ConditionMismatch() {
-        bool result = new ObjectHasTag().Evaluate(new(), new DummySubevent(), new JsonObject().LoadFromString("""
-            {
-                "condition": "not-a-condition"
-            }
-            """), new DummyContext(), new());
-
-        Assert.False(result);
-    }
-
     [ClearRPGLAfterTest]
     [DefaultMock]
     [Fact(DisplayName = "object does have tag")]
     public void ObjectDoesHaveTag() {
         RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", TestUtils.USER_ID);
         rpglObject.AddTag("test_tag");
+        RPGLContext context = new DummyContext().Add(rpglObject);
+        RPGLEffect rpglEffect = new();
+        Subevent subevent = new DummySubevent()
+            .SetSource(rpglObject);
 
-        bool result = new ObjectHasTag().Evaluate(
-            new(),
-            new DummySubevent().SetSource(rpglObject),
-            new JsonObject().LoadFromString("""
-                {
-                    "condition": "object_has_tag",
-                    "object": {
-                        "from": "subevent",
-                        "object": "source",
-                        "as_origin": false
-                    },
-                    "tag": "test_tag"
-                }
-                """),
-            new DummyContext(),
-            new()
-        );
+        ObjectHasTag condition = new ObjectHasTag().Clone();
 
-        Assert.True(result);
+        JsonObject conditionJson = new JsonObject().LoadFromString("""
+            {
+                "condition": "object_has_tag",
+                "object": {
+                    "from": "subevent",
+                    "object": "source"
+                },
+                "tag": "test_tag"
+            }
+            """);
+
+        ConditionState.StateData result;
+
+        result = condition.conditionSteps[0](rpglEffect, subevent, conditionJson, context);
+        Assert.Null(result.conditionDependency);
+        Assert.NotNull(result.subeventDependency);
+        Assert.False(result.stepCompleted);
+        Assert.True(result.subeventDependency is GetObjectTags);
+
+        (result.subeventDependency as GetObjectTags)
+            .AddObjectTag("test_tag");
+
+        result = condition.conditionSteps[0](rpglEffect, subevent, conditionJson, context);
+        Assert.Equal(new ConditionState.StateData() { conditionDependency = null, subeventDependency = null, stepCompleted = true }, result);
+
+        Assert.True(condition.evaluation);
     }
 
     [ClearRPGLAfterTest]
@@ -55,26 +59,36 @@ public class ObjectHasTagTest {
     [Fact(DisplayName = "object does not have tag")]
     public void ObjectDoesNotHaveTag() {
         RPGLObject rpglObject = RPGLFactory.NewObject("test:dummy", TestUtils.USER_ID);
+        RPGLContext context = new DummyContext().Add(rpglObject);
+        RPGLEffect rpglEffect = new();
+        Subevent subevent = new DummySubevent()
+            .SetSource(rpglObject);
 
-        bool result = new ObjectHasTag().Evaluate(
-            new(),
-            new DummySubevent().SetSource(rpglObject),
-            new JsonObject().LoadFromString("""
-                {
-                    "condition": "object_has_tag",
-                    "object": {
-                        "from": "subevent",
-                        "object": "source",
-                        "as_origin": false
-                    },
-                    "tag": "test_tag"
-                }
-                """),
-            new DummyContext(),
-            new()
-        );
+        ObjectHasTag condition = new ObjectHasTag().Clone();
 
-        Assert.False(result);
+        JsonObject conditionJson = new JsonObject().LoadFromString("""
+            {
+                "condition": "object_has_tag",
+                "object": {
+                    "from": "subevent",
+                    "object": "source"
+                },
+                "tag": "test_tag"
+            }
+            """);
+
+        ConditionState.StateData result;
+
+        result = condition.conditionSteps[0](rpglEffect, subevent, conditionJson, context);
+        Assert.Null(result.conditionDependency);
+        Assert.NotNull(result.subeventDependency);
+        Assert.False(result.stepCompleted);
+        Assert.True(result.subeventDependency is GetObjectTags);
+
+        result = condition.conditionSteps[0](rpglEffect, subevent, conditionJson, context);
+        Assert.Equal(new ConditionState.StateData() { conditionDependency = null, subeventDependency = null, stepCompleted = true }, result);
+
+        Assert.False(condition.evaluation);
     }
 
 };
